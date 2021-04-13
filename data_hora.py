7e836b40cba187d3216c66205c67ed9a64f6f6fa
Thiago Tuissi Paião <81596921+ThiagoTuissiPaiao@users.noreply.github.com>
2021-04-12 23:02:27 -0300

    Add files via upload

---
+++ data_hora.py
@@ -0,0 +1,11 @@
+import datetime
+
+data = datetime.date.today()
+ano = datetime.date.today().year
+dia = datetime.date.today().day
+mes = datetime.date.today().month
+
+print(f"Data atual:  {data}")
+print(f"Você esta no dia: {dia}")
+print(f"Você esta no dia: {mes}")
+print(f"Você esta no ano: {ano}")
